a922968aecfe9f211f2bfb60711f9deef15c40e2
ENKI <amit.khanna.1082@gmail.com>
2026-06-28 22:01:30 +0700

    Add Logger class for logging functionality

---
+++ Rig-S/include/Logger.h
@@ -0,0 +1,34 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <string>
+#include <fstream>
+
+class Logger
+{
+public:
+
+    Logger();
+
+    ~Logger();
+
+    bool open(const std::string& filename);
+
+    void close();
+
+    void info(const std::string& message);
+
+    void warning(const std::string& message);
+
+    void error(const std::string& message);
+
+private:
+
+    std::ofstream logFile;
+
+    void write(
+        const std::string& level,
+        const std::string& message);
+};
+
+#endif // LOGGER_H
