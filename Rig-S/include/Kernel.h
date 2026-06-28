3a249252d73eb74cf5984c4c5d3923b05b09d645
ENKI <amit.khanna.1082@gmail.com>
2026-06-28 23:21:01 +0700

    Add Kernel class with initialization and management methods

---
+++ Rig-S/include/Kernel.h
@@ -0,0 +1,44 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#include "Config.h"
+#include "Logger.h"
+#include "Statistics.h"
+#include "Dashboard.h"
+#include "Timer.h"
+#include "StateManager.h"
+#include "TaskScheduler.h"
+#include "PluginManager.h"
+#include "ModuleManager.h"
+#include "EventManager.h"
+#include "SystemInfo.h"
+#include "Version.h"
+
+class Kernel
+{
+public:
+
+    Kernel();
+
+    bool initialize();
+
+    void run();
+
+    void shutdown();
+
+private:
+
+    Config config;
+    Logger logger;
+    Statistics statistics;
+    Dashboard dashboard;
+    Timer timer;
+    StateManager stateManager;
+    TaskScheduler scheduler;
+    PluginManager pluginManager;
+    ModuleManager moduleManager;
+    EventManager eventManager;
+    SystemInfo systemInfo;
+};
+
+#endif // KERNEL_H
